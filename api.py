8bf6b30c8c599854221261beedce51c3a6b7930e
kumuman <112801481+kumuman@users.noreply.github.com>
2024-05-03 19:24:36 +0800

    Update api.py

--- api.py
+++ api.py
@@ -49,12 +49,9 @@ async def create_item(request: Request):
 
 
 if __name__ == '__main__':
+    #此处修改为你本地存放模型的路径
     tokenizer = AutoTokenizer.from_pretrained("ChatGLM3-6b", trust_remote_code=True)
     model = AutoModel.from_pretrained("ChatGLM3-6b", trust_remote_code=True).cuda()
-    # 多显卡支持，使用下面三行代替上面两行，将num_gpus改为你实际的显卡数量
-    # model_path = "THUDM/chatglm2-6b"
-    # tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-    # model = load_model_on_gpus(model_path, num_gpus=2)
     model.eval()
 
     uvicorn.run(app, host='localhost', port=8000, workers=1)
